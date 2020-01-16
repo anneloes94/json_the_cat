d4653746225742a5133cb78186e5c76db2a6ccb9
Anneloes <anneloes9405@hotmail.com>
2020-01-16 21:44:38 +0000

    made mocha test for breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,14 +1,18 @@
+// RETURNS DESCRIPTION
 const request = require('request');
 const URL = "https://api.thecatapi.com/v1/breeds/search?q=";
-const breedname = process.argv.slice(2);
 
-request(`${URL}${breedname}`, (error, response, body) => {
-  console.log(error);
-  if (body = []) {
-    return console.log("Noooo!");
-  } else {
-    const data = JSON.parse(body);
-    console.log(typeof body);
-    console.log(data[0].description);
-  }
-});
+const fetchBreedDescription = (breedName, callback) => {
+  request(`${URL}${breedName}`, (error, response, body) => {
+    if (body.length < 3 || error) {
+      // console.log(error)
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      const description = data[0].description.trim();
+      callback(null, description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
