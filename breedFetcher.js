472b3b379bfc3fddc8f7f4bd88624fd48d71396f
Anneloes <anneloes9405@hotmail.com>
2020-01-16 19:10:08 +0000

    minor eslint fixes

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,14 @@
-const request = require('request')
-const URL = "https://api.thecatapi.com/v1/breeds/search?q="
-const breedname = process.argv.slice(2)
+const request = require('request');
+const URL = "https://api.thecatapi.com/v1/breeds/search?q=";
+const breedname = process.argv.slice(2);
 
 request(`${URL}${breedname}`, (error, response, body) => {
+  console.log(error);
   if (body = []) {
-    return console.log("Noooo!")
+    return console.log("Noooo!");
   } else {
-    const data = JSON.parse(body)
+    const data = JSON.parse(body);
     console.log(typeof body);
-    console.log(data[0].description)
+    console.log(data[0].description);
   }
 });
-
-
-
-
-// print out body conten
-
-// make into JS object
-// JSON.parse
-
-// access first entry in data array
-//and print out description
